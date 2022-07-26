8571fcbb8938d027cdf974c9086edcb9886a8c37
Ryan <thisianemailaddressthatis@gmail.com>
2022-07-26 13:14:08 -0500

    added function for met museum. It will fetch and return an image, artist name and date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,46 @@
 // Met museum API. Makes a url based on search input and recieves data. Look in console.
-var getSearchProps = function () {
+var getMuseumOne = function () {
   const input = document.getElementById("search_museum");
   const q = input.value;
-  var artUrl =
-    "https://collectionapi.metmuseum.org/public/collection/v1/search?q=" +
+  let selectedValue = "1";
+  var objectUrl =
+    "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=" +
     encodeURIComponent(q);
-  console.log(artUrl);
-  fetch(artUrl).then(function (response) {
+  //console.log(artUrl);
+  fetch(objectUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
         console.log("data", data);
+        const objectIds = data.objectIDs || [];
+        if (objectIds.length) {
+          const artObjects = [];
+          for (var i = 0; i < objectIds.length; i++) {
+            var artUrl =
+              "https://collectionapi.metmuseum.org/public/collection/v1/objects/" +
+              objectIds[i];
+            fetch(artUrl).then(function (response) {
+              if (response.ok) {
+                response.json().then(function (data) {
+                  console.log("data", data);
+                  if (data.primaryImage !== "") {
+                    artObjects.push({
+                      title: data.title,
+                      primaryImage: data.primaryImage,
+                      primaryImageSmall: data.primaryImageSmall,
+                      additionalImages: data.additionalImages,
+                      objectDate: data.objectDate,
+                      artistDisplayName: data.artistDisplayName,
+                    });
+                  }
+                });
+              } else {
+                alert("Error: " + response.statusText);
+              }
+            });
+          }
+          console.log(artObjects);
+          return artObjects;
+        }
       });
     } else {
       alert("Error: " + response.statusText);
@@ -20,5 +51,15 @@ var getSearchProps = function () {
 $("#search-items").on("click", function (event) {
   event.preventDefault();
   console.log("test");
-  getSearchProps();
+  if (selectedValue === "1") {
+    getMuseumOne();
+  }
+});
+
+$("select").on("change", function handleChange(event) {
+  event.preventDefault();
+  $("#search-items").removeAttr("disabled");
+  console.log(event.target.value);
+  selectedValue = event.target.value;
+  //getMuseumOne();
 });
