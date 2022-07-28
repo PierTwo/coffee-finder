54ba1af818ac3e040c0aaf5cc7de8cc0e7636624
Ryan <thisianemailaddressthatis@gmail.com>
2022-07-27 19:04:36 -0500

    added carousel for both museums. needs some tweaking

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
 $(document).ready(function () {
-  $('.carousel').carousel();
+  $(".carousel").carousel();
 });
 
 // Met museum API. Makes a url based on search input and recieves data. Look in console.
-const input = $("#search_museum")
-
+const input = $("#search_museum");
 
 var getMuseumOne = function (q) {
   let selectedValue = "1";
@@ -12,51 +11,65 @@ var getMuseumOne = function (q) {
     "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=" +
     encodeURIComponent(q);
   //console.log(artUrl);
-  fetch(objectUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log("data", data);
-        const objectIds = data.objectIDs || [];
-        if (objectIds.length) {
-          const artObjects = [];
-          for (var i = 0; i < objectIds.length; i++) {
-            var artUrl =
-              "https://collectionapi.metmuseum.org/public/collection/v1/objects/" +
-              objectIds[i];
-            fetch(artUrl).then(function (response) {
-              if (response.ok) {
-                response.json().then(function (data) {
-                  console.log("data", data);
-                  if (data.primaryImage !== "") {
-                    artObjects.push({
-                      title: data.title,
-                      primaryImage: data.primaryImage,
-                      primaryImageSmall: data.primaryImageSmall,
-                      additionalImages: data.additionalImages,
-                      objectDate: data.objectDate,
-                      artistDisplayName: data.artistDisplayName,
+  fetch(objectUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          // console.log("data", data);
+          const objectIds = data.objectIDs || [];
+          if (objectIds.length) {
+            const artObjects = [];
+            for (var i = 0; i < objectIds.length; i++) {
+              var artUrl =
+                "https://collectionapi.metmuseum.org/public/collection/v1/objects/" +
+                objectIds[i];
+              fetch(artUrl)
+                .then(function (response) {
+                  if (response.ok) {
+                    response.json().then(function (data) {
+                      // console.log("data: ", data);
+                      if (data.primaryImage !== "") {
+                        var slider = $(".carousel");
+                        slider.carousel();
+                        // console.log("image ", data.primaryImage);
+                        const slide =
+                          '<a class="image carousel-item active" href="#three!"><img src=' +
+                          data.primaryImage +
+                          "></a>";
+                        // console.log("slide: ", slide);
+                        slider.append(slide);
+
+                        if (slider.hasClass("initialized")) {
+                          slider.removeClass("initialized");
+                        }
+
+                        slider.carousel();
+                      }
                     });
+                  } else {
+                    // alert("Error: " + response.statusText);
+                    console.error("Not OK");
                   }
+                })
+                .catch(function (error) {
+                  console.log("Inner Fetch 404 Not Found : " + error);
                 });
-              } else {
-                alert("Error: " + response.statusText);
-              }
-            });
+            }
           }
-          console.log(artObjects);
-          return artObjects;
-        }
-      });
-    } else {
-      alert("Error: " + response.statusText);
-    }
-  });
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      console.log("Outer fetch 404 Not Found : " + error);
+    });
 };
 
 function chicagoArt(q) {
-  // $(".carousel").empty();
-
-  fetch(`https://api.artic.edu/api/v1/artworks/search?q=${q}&limit=5&fields=id,title,image_id,artist_title,thumbnail,date_display,place_of_origin,date_qualifier_title`)
+  fetch(
+    `https://api.artic.edu/api/v1/artworks/search?q=${q}&limit=5&fields=id,title,image_id,artist_title,thumbnail,date_display,place_of_origin,date_qualifier_title`
+  )
     .then(function (response) {
       console.log(response);
       return response.json();
@@ -67,18 +80,39 @@ function chicagoArt(q) {
     });
 
   function chicagoArtResults(results) {
-    console.log(results.length)
+    console.log(results.length);
     for (let i = 0; i < results.length; i++) {
-      var img = $(`#c${i}`).attr("src", `https://www.artic.edu/iiif/2/${results[i].image_id}/full/843,/0/default.jpg`)
-      console.log(img)
+      /*
+      var img = $(`#c${i}`).attr(
+        "src",
+        `https://www.artic.edu/iiif/2/${results[i].image_id}/full/843,/0/default.jpg`
+      );
+      */
+      if (results[i].image_id !== "") {
+        var slider = $(".carousel");
+        slider.carousel();
+        // console.log("image ", data.primaryImage);
+        const slide =
+          '<a class="image carousel-item active" href="#three!"><img src=https://www.artic.edu/iiif/2/' +
+          results[i].image_id +
+          "/full/843,/0/default.jpg /></a>";
+        // console.log("slide: ", slide);
+        slider.append(slide);
+
+        if (slider.hasClass("initialized")) {
+          slider.removeClass("initialized");
+        }
+
+        slider.carousel();
+      }
+      // console.log(img);
       // carouselArray = ["#one!", "#two!", "#three!", "#four!", "#five!"]
       // var imgHTML = `<a class="carousel-item" href="${carouselArray[i]}"><img src=""></a>`
       // console.log(imgHTML);
       // $(".carousel").append(imgHTML);
-    };
-  };
-};
-
+    }
+  }
+}
 
 $("select").on("change", function handleChange(event) {
   event.preventDefault();
@@ -92,11 +126,11 @@ $("select").on("change", function handleChange(event) {
     let q = input.val();
 
     if (selectedValue === "1") {
-      console.log("Met")
+      // console.log("Met");
       getMuseumOne(q);
     } else if (selectedValue === "2") {
       console.log("Chicago");
       chicagoArt(q);
-    };
+    }
   });
 });
