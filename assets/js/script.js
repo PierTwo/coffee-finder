b2e16a2b66efb16b7415d4cf566e0d2353635ac7
Ryan <thisianemailaddressthatis@gmail.com>
2022-07-29 11:41:47 -0500

    fixed met api images being blank. added hrefs to images.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,44 @@
 var slider = $(".carousel");
 const searchInput = $("#search_museum");
 
-// Met museum API. Makes a url based on search input and recieves data. Look in console.
+// Met museum API. Makes a url based on search input and recieves data.
 
 var getMuseumOne = function () {
-  var objectUrl = `https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=${searchInput.val()}`
+  var objectUrl = `https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=${searchInput.val()}`;
   fetch(objectUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       returnObjects(data.objectIDs);
+      console.log(data);
     });
 
   function returnObjects(objectIDs) {
-    for (let i = 0; i < 10; i++) {
-      fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectIDs[i]}`)
+    for (let i = 0; i < objectIDs.length; i++) {
+      fetch(
+        `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectIDs[i]}`
+      )
         .then(function (response) {
-          return response.json()
+          return response.json();
         })
         .then(function (data) {
-          console.log(data)
-          var carouselItem = `<a class="image carousel-item" href="#${i}!"><img src="${data.primaryImage}"></a>`;
-          slider.append(carouselItem);
-          if (slider.children().length === 10) {
-            slider.carousel();
-          };
+          if (data.primaryImage !== "") {
+            var carouselItem = `<a class="image carousel-item" href="${data.objectURL}" target="_blank"><img src="${data.primaryImage}"></a>`;
+            slider.append(carouselItem);
+            if (slider.children().length === 10) {
+              slider.carousel();
+            }
+          }
         });
-    };
-  };
+    }
+  }
 };
 
-
 function chicagoArt() {
-  fetch(`https://api.artic.edu/api/v1/artworks/search?q=${searchInput.val()}&limit=10&fields=id,title,image_id,artist_title,thumbnail,date_display,place_of_origin,date_qualifier_title`)
+  fetch(
+    `https://api.artic.edu/api/v1/artworks/search?q=${searchInput.val()}&limit=10&fields=id,title,image_id,artist_title,thumbnail,date_display,place_of_origin,date_qualifier_title`
+  )
     .then(function (response) {
       return response.json();
     })
@@ -46,11 +51,11 @@ function chicagoArt() {
       if (results[i].image_id !== "") {
         var carouselItem = `<a class="image carousel-item" href="#${i}!"><img src="https://www.artic.edu/iiif/2/${results[i].image_id}/full/843,/0/default.jpg"></a>`;
         slider.append(carouselItem);
-      };
-    };
+      }
+    }
     slider.carousel();
-  };
-};
+  }
+}
 
 var selectedValue;
 
@@ -60,14 +65,13 @@ $("select").change(function handleChange(event) {
   selectedValue = event.target.value;
 
   $("#search-items").removeAttr("disabled");
-
 });
 
 $("#search-items").on("click", function (event) {
   event.preventDefault();
   if (slider.hasClass("initialized")) {
     slider.removeClass("initialized");
-  };
+  }
   slider.empty();
   switch (selectedValue) {
     case "1":
@@ -76,5 +80,5 @@ $("#search-items").on("click", function (event) {
     case "2":
       chicagoArt();
       break;
-  };
+  }
 });
