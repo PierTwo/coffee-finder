c073cfc548f1480975657760f31dcce81059b94c
Jordan <jordan.b3360@gmail.com>
2022-07-28 20:11:55 -0500

    added local storage to save the art searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,173 @@
+var slider = $(".carousel");
+$(document).ready(function () {
+  slider.carousel();
+});
+let saveTheImageSearch = JSON.parse(localStorage.getItem("savedImages") || "[]");
+
 // Met museum API. Makes a url based on search input and recieves data. Look in console.
-var getMuseumOne = function () {
-  const input = document.getElementById("search_museum");
-  const q = input.value;
+const input = $("#search_museum");
+
+var getMuseumOne = function (q) {
   let selectedValue = "1";
   var objectUrl =
     "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=" +
     encodeURIComponent(q);
   //console.log(artUrl);
-  fetch(objectUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log("data", data);
-        const objectIds = data.objectIDs || [];
-        if (objectIds.length) {
-          const artObjects = [];
-          for (var i = 0; i < objectIds.length; i++) {
-            var artUrl =
-              "https://collectionapi.metmuseum.org/public/collection/v1/objects/" +
-              objectIds[i];
-            fetch(artUrl).then(function (response) {
-              if (response.ok) {
-                response.json().then(function (data) {
-                  console.log("data", data);
-                  if (data.primaryImage !== "") {
-                    artObjects.push({
-                      title: data.title,
-                      primaryImage: data.primaryImage,
-                      primaryImageSmall: data.primaryImageSmall,
-                      additionalImages: data.additionalImages,
-                      objectDate: data.objectDate,
-                      artistDisplayName: data.artistDisplayName,
+  fetch(objectUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          // console.log("data", data);
+          const objectIds = data.objectIDs || [];
+          if (objectIds.length) {
+            for (var i = 0; i < 10; i++) {
+         
+              var artUrl =
+                "https://collectionapi.metmuseum.org/public/collection/v1/objects/" +
+                objectIds[i];
+              fetch(artUrl)
+                .then(function (response) {
+                  if (response.ok) {
+                    response.json().then(function (data) {
+                      if (data.primaryImage !== "") {
+                        // var slider = $(".carousel");
+                        // slider.carousel();
+                        // console.log("image ", data.primaryImage);
+                        const slide =
+                          '<a class="image carousel-item active" href="#three!"><img src=' +
+                          data.primaryImage +
+                          "></a>";
+                        // console.log("slide: ", slide);
+                        slider.append(slide);
+
+                        if (slider.hasClass("initialized")) {
+                          slider.removeClass("initialized");
+                        } 
+                          slider.carousel();
+                        
+                          
+                        
+                      }
                     });
+                  } else {
+                    // alert("Error: " + response.statusText);
+                    console.error("Not OK");
                   }
+                })
+                .catch(function (error) {
+                  console.log("Inner Fetch 404 Not Found : " + error);
                 });
-              } else {
-                alert("Error: " + response.statusText);
-              }
-            });
+            }
           }
-          console.log(artObjects);
-          return artObjects;
-        }
-      });
-    } else {
-      alert("Error: " + response.statusText);
-    }
-  });
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      console.log("Outer fetch 404 Not Found : " + error);
+    });
 };
 
-$("#search-items").on("click", function (event) {
-  event.preventDefault();
-  console.log("test");
-  if (selectedValue === "1") {
-    getMuseumOne();
+function chicagoArt(q) {
+  fetch(
+    `https://api.artic.edu/api/v1/artworks/search?q=${q}&limit=5&fields=id,title,image_id,artist_title,thumbnail,date_display,place_of_origin,date_qualifier_title`
+  )
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      chicagoArtResults(data.data);
+    });
+
+  function chicagoArtResults(results) {
+    console.log(results.length);
+    for (let i = 0; i < 10; i++) {
+     
+      /*
+      var img = $(`#c${i}`).attr(
+        "src",
+        `https://www.artic.edu/iiif/2/${results[i].image_id}/full/843,/0/default.jpg`
+      );
+      */
+      if (results[i].image_id !== "") {
+        console.log(results[i].image_id);
+        // var slider = $(".carousel");
+        // slider.carousel();
+        // console.log("image ", data.primaryImage);
+        const slide =
+          '<a class="image carousel-item active" href="#three!"><img src=https://www.artic.edu/iiif/2/' +
+          results[i].image_id +
+          "/full/843,/0/default.jpg /></a>";
+        // console.log("slide: ", slide);
+        slider.append(slide);
+
+        if (slider.hasClass("initialized")) {
+          slider.removeClass("initialized");
+        }
+        
+          slider.carousel();
+      
+        
+      }
+      // console.log(img);
+      // carouselArray = ["#one!", "#two!", "#three!", "#four!", "#five!"]
+      // var imgHTML = `<a class="carousel-item" href="${carouselArray[i]}"><img src=""></a>`
+      // console.log(imgHTML);
+      // $(".carousel").append(imgHTML);
+    }
   }
-});
+}
+
+
+function saveImageSearch() {
+  let userInput = input.val();
+  saveTheImageSearch.push(userInput);
+
+  localStorage.setItem("savedImages", JSON.stringify(saveTheImageSearch));
+ 
+};
+
+function savedArtSearches() {
+  $("#saved-art-searches").html("");
+
+  for (let i = 0; i < saveTheImageSearch.length; i++) {
+    var saveSearchBtn = $("<button></button>").text(saveTheImageSearch[i]);
+    saveSearchBtn.addClass("btn waves-effect waves-light s12 m6");
+    $("#saved-art-searches").append(saveSearchBtn);
+    saveSearchBtn.on("click", function(e){
+      getMuseumOne(saveTheImageSearch[i]);
+      chicagoArt(saveTheImageSearch[i]);
+    });
+  }
+  // btn waves-effect waves-light s12 m6
+};
+
+savedArtSearches();
+
+
 
 $("select").on("change", function handleChange(event) {
   event.preventDefault();
+
   $("#search-items").removeAttr("disabled");
-  console.log(event.target.value);
-  selectedValue = event.target.value;
-  //getMuseumOne();
+
+  let selectedValue = event.target.value;
+
+  $("#search-items").on("click", function (event) {
+    event.preventDefault();
+    $(".carousel").empty();
+    let q = input.val();
+
+    if (selectedValue === "1") {
+      // console.log("Met");
+      getMuseumOne(q);
+    } else if (selectedValue === "2") {
+      console.log("Chicago");
+      chicagoArt(q);
+    }
+    saveImageSearch();
+  });
 });
